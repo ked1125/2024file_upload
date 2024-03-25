9741dfa23f495519fe1ae9803d6a0f4315a6cf63
ked1125 <ked1125@naver.com>
2024-03-25 15:22:17 +0900

    240325_5th_server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+// require는 가져오기만 하는거고.. config는 설정까지 해준다...?:/
 
 const { upload } = require("./middleweares/imgeUpload");
 
@@ -17,6 +18,7 @@ const server = async function () {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("db연결완료");
     app.use(express.json());
+    // 미들웨어_app.____는 전부 미들웨어라고 보면됨!!!! 미들웨어를 앱에 붙여주는 역할을 하는게 app.___() 함수..
 
     app.post("/upload", upload.single("image"), async function (req, res) {
       // <form><input tytpe=file name=image></form>
