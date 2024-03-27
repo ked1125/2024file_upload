27df65c06f1e1c97e0f83345360892736d41b004
ked1125 <ked1125@naver.com>
2024-03-27 11:36:37 +0900

    20240327_1st&2nd_userRouter

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const app = express();
 const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+// require는 가져오기만 하는거고.. config는 설정까지 해준다...?:/
 
-const { upload } = require("./middleweares/imgeUpload");
+// const { upload } = require("./middleweares/imgeUpload");
+const { imageRouter } = require("./routes/imageRouter");
+const { userRouter } = require("./routes/userRouter");
 
 // app.use(express.static("uploads"));
 // http://localhost:3000/abc.jpg
@@ -17,21 +20,15 @@ const server = async function () {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("db연결완료");
     app.use(express.json());
+    // 미들웨어_app.____는 전부 미들웨어라고 보면됨!!!! 미들웨어를 앱에 붙여주는 역할을 하는게 app.___() 함수..
 
-    app.post("/upload", upload.single("image"), async function (req, res) {
-      // <form><input tytpe=file name=image></form>
-      try {
-        console.log(req.file);
-        return res.send(req.file);
-      } catch (error) {
-        return res.status(500).send({ error: error.message });
-        // 여기서 오는 error은 catch의 error에서 옴..:/
-      }
-    });
+    app.use("/upload", imageRouter);
+    app.use("/user", userRouter);
+    // userRouter파일에서 module.exports해온걸 받아서 사용하기 위한 코드
+    app.listen(3000);
   } catch (error) {
     console.log("db연결실패");
   }
 };
 
-app.listen(3000);
 server();
